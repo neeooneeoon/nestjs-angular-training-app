68f5572503f7140670a3fe6271547b5e4a46edc8
Ngoc Nguyen <30312571+neeooneeoon@users.noreply.github.com>
2021-10-07 13:59:01 +0700

    Update validation.exception.ts

--- rest-api/src/filters/validation.exception.ts
+++ rest-api/src/filters/validation.exception.ts
@@ -1,6 +1,5 @@
 import { BadRequestException, Catch } from "@nestjs/common";
 
-@Catch(ValidationException)
 export class ValidationException extends BadRequestException {
     constructor(public validationErrors: string[]) {
         super();
